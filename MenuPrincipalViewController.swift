dd86f0388750d764be29feda0473460c813ae7ea
Mac <mac@MacBook-Pro-de-Mac.local>
2018-07-03 05:12:52 -0500

    v2.7 listado sin imagenes
    https://www.youtube.com/watch?v=OE8YDGDoHXk
    tutorial

--- MenuPrincipalViewController.swift
+++ MenuPrincipalViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class MenuPrincipalViewController: UIViewController ,UICollectionViewDataSource,UICollectionViewDelegate{
     
@@ -14,6 +16,11 @@ class MenuPrincipalViewController: UIViewController ,UICollectionViewDataSource,
     @IBOutlet weak var CollectionView: UICollectionView!
     
     @IBOutlet weak var btnMenuButton: UIBarButtonItem!
+    
+   
+    
+    var listado = [[String : AnyObject]]()
+    
     
     let products = ["foto 1","foto 2","foto 3","foto 4"]
     let productsImages : [UIImage] = [UIImage(named: "ic_pedido")!,
@@ -31,22 +38,51 @@ class MenuPrincipalViewController: UIViewController ,UICollectionViewDataSource,
         btnMenuButton.target = revealViewController()
         btnMenuButton.action = #selector(SWRevealViewController.revealToggle(_:))
         
+        self.data()
+        
+        
+        
     }
 
+    func data(){
+        //llamada al rest
+        let url = "https://fakebucks.azurewebsites.net/api/products/all"
+        
+        request(url).validate().responseJSON{(response) in
+            if((response.result.value) != nil){
+                let jsondata = JSON(response.result.value!)
+                print(jsondata)
+                if let da = jsondata["result"].arrayObject{
+                    self.listado = da as! [[String:AnyObject]]
+                    
+                }
+                if self.listado.count > 0 {
+                    self.CollectionView.reloadData()
+                    
+                }
+                
+            }
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return products.count
+        return listado.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "Celda", for: indexPath) as! ProductsCVCell
-        celda.txtNombre.text = products[indexPath.item]
-        celda.txtPrecio.text = products[indexPath.item]
-        celda.imgProduct.image = productsImages[indexPath.item]
+        let lista = listado[indexPath.row]
+        
+        celda.txtNombre.text = lista["nombre"] as? String
+        
+        celda.txtPrecio.text =  lista["precio"] as? String
+      //  celda.imgProduct.image = productsImages[indexPath.item]
         
         return celda
     }
