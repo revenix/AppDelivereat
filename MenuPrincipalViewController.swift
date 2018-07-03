1204d2092cd5791ceb67bb40fea544b0ee7b0fe6
Mac <mac@MacBook-Pro-de-Mac.local>
2018-07-03 02:41:44 -0500

    v2.1 menuSlideLeft Complete

--- MenuPrincipalViewController.swift
+++ MenuPrincipalViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 
 class MenuPrincipalViewController: UIViewController {
 
+    @IBOutlet weak var btnMenuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        btnMenuButton.target = revealViewController()
+        btnMenuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +24,6 @@ class MenuPrincipalViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
